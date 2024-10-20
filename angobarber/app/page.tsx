0923484a1a5af21754900007d23eb6d6e3f6ff82
Macedo Francisco <macedolb53@gmail.com>
2024-10-20 04:46:39 +0200

    chore: add shadcn theme

--- angobarber/app/page.tsx
+++ angobarber/app/page.tsx
@@ -3,10 +3,6 @@ import { Button } from "./components/ui/button"
 const Home = () => {
   return (
     <div>
-      <h1 className="mb-5 bg-red-400 text-red-500">
-        Ola vamos comecar o nosso projecto
-      </h1>
-
       <Button>Click</Button>
     </div>
   )
